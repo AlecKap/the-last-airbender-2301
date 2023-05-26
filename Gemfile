4618d9d27ba3599795ef4bfcc07bae0432293cd5
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-26 13:40:43 -0600

    Setup db and service.

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ group :test do
   gem "rspec-rails"
   gem "capybara"
   gem "launchy"
+  gem 'shoulda-matchers'
   gem "simplecov"
   # gem "webmock"
   # gem "vcr"
@@ -69,7 +70,7 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
-
+  gem 'rubocop-rails'
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
---
+++ spec/services/tlab_service_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe TlabService do
+  context 'class methods' do
+    context 'members_by_nation' do
+      it 'returns data of members affiliated with a particular nation' do
+        query = "Fire+Nation"
+        members = TlabService.new.members_by_nation(query)
+        member = members.first
+
+        expect(members).to be_an(Array)
+        expect(member).to be_an(Hash)
+
+        expect(member).to have_key(:_id)
+        expect(member).to have_key(:allies)
+        expect(member).to have_key(:enemies)
+        expect(member).to have_key(:photoUrl)
+        expect(member).to have_key(:name)
+        expect(member).to have_key(:affiliation)
+
+        expect(member[:_id]).to be_a(String)
+        expect(member[:allies]).to be_an(Array)
+        expect(member[:enemies]).to be_an(Array)
+        expect(member[:photoUrl]).to be_a(String)
+        expect(member[:name]).to be_a(String)
+        expect(member[:affiliation]).to be_a(String)
+      end
+    end
+  end
+end
